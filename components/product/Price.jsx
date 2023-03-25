1148e59dd3f28c74b759a05e7e941d9268c7e2a4
szabolcsfuli <szabolcsfuli.dev@gmail.com>
2023-03-25 23:23:36 +0100

    clean up

--- components/product/Price.jsx
+++ components/product/Price.jsx
@@ -4,7 +4,7 @@ import Styles from '../../styles/styles'
 export default ({ price, cls }) => {
   return (
     <Text style={cls ? Styles.totalPrice : Styles.price}>
-      ${price.toFixed(2)}dddd
+      ${price.toFixed(2)}
     </Text>
   )
 }
